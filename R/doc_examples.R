896a87937f085bb2fc948b75cf59167d1ea973b9
Alberto44 <garre.alberto@gmail.com>
2015-10-07 17:55:27 +0200

    Added example data set

---
+++ R/doc_examples.R
@@ -0,0 +1,15 @@
+#' Inactivation of a Microorganism
+#'
+#' Example of the inactivation process of a microorganism.
+#'
+#' \itemize{
+#'   \item time. Time in seconds of the measurement.
+#'   \item N. Number of microorganism
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name inactivation_example
+#' @usage data(inactivation_example)
+#' @format A data frame with 19 rows and 2 variables
+NULL
