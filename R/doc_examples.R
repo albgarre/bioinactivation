4ff663f245b2d9c0e61b283117849edfaa0ea95c
Alberto44 <garre.alberto@gmail.com>
2015-10-15 16:53:18 +0200

    Added example of isothermal inactivation.

--- R/doc_examples.R
+++ R/doc_examples.R
@@ -1,6 +1,7 @@
-#' Inactivation of a Microorganism
+#' Example Dynamic Inactivation of a Microorganis
 #'
-#' Example of the inactivation process of a microorganism.
+#' Example of experimental data of the dynamic inactivation process of a
+#' microorganism.
 #'
 #' \itemize{
 #'   \item time. Time in seconds of the measurement.
@@ -12,4 +13,22 @@
 #' @name inactivation_example
 #' @usage data(inactivation_example)
 #' @format A data frame with 19 rows and 2 variables
+NULL
+
+#' Example Isothermal Inactivation of a Microorganis
+#'
+#' Example of experimental data for an isothermal process of a
+#' microorganism
+#'
+#' \itemize{
+#'   \item time: Time in seconds of the measurement.
+#'   \item temp: Temperature at which the experiment was made.
+#'   \item log_diff: Logarithmic difference.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name isothermal_example
+#' @usage data(isothermal_example)
+#' @format A data frame with 36 rows and 3 variables
 NULL
