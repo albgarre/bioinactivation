9ca180556da085cb7c3a6dc5a1d9bf3722bd3def
Alberto44 <garre.alberto@gmail.com>
2016-01-16 20:10:05 +0100

    Some changes to documentation.

--- R/predict_MCMC.R
+++ R/predict_MCMC.R
@@ -5,19 +5,27 @@
 #' performed using \code{\link{fit_inactivation_MCMC}} calculates
 #' probability intervals at each time point using a Monte Carlo method.
 #'
-#' @param MCMC_fit An object of class \code{FitInactivationMCMC} as generated
-#'        by \code{\link{fit_inactivation_MCMC}}.
+#' @param fit_object An object of classes \code{FitInactivationMCMC},
+#'        \code{IsoFitInactivation}
+#'
+#' @param temp_profile a data frame indicating the temperature profile to use
+#'        for the simulation.
+#'
 #' @param n_simulations a numeric indicating how many Monte Carlo simulations
 #'        to perform. \code{100} by default.
+#'
 #' @param times numeric vector specifying the time points when results are
 #'        desired. If \code{NULL}, the times in \code{MCMC_fit$best_prediction}
 #'        are used. \code{NULL} by default.
+#'
 #' @param quantiles numeric vector indicating the quantiles to calculate in
 #'        percentage. By default, it is set to c(2.5, 97.5) which generates a
 #'        prediction interval with confidence 0.95. If \code{NULL}, the quantiles
 #'        are not calculated and all the simulations are returned.
 #'
-#' @importFrom dplyr sample_n
+#' @param additional_pars Additional parameters not included in the adjustment (e.g.
+#'        the initial number of microorganism in an isothermal fit).
+#'
 #' @importFrom stats quantile median
 #'
 #' @return A data frame of class \code{PredInactivationMCMC}. On its first column,
@@ -29,71 +37,49 @@
 #'
 #' @export
 #'
-#' @examples
-#' ## EXAMPLE 1 ------
-#' data(dynamic_inactivation)  # The example data set is used.
-#'
-#' simulation_model <- "Peleg"  # Peleg's model will be used
-#'
-#' model_data <- get_model_data(simulation_model)
-#' model_data$parameters  # Set the model parameters
-#'
-#' dummy_temp <- data.frame(time = c(0, 1.25, 2.25, 4.6),
-#'                          temperature = c(70, 105, 105, 70))  # Dummy temp. profile
-#'
-#' ## Set known parameters and initial points/bounds for unknown ones
-#'
-#' known_params = c(temp_crit = 100)
-#'
-#' starting_points <- c(n = 1, k_b = 0.25, N0 = 1e+05)
-#' upper_bounds <- c(n = 2, k_b = 1, N0 = 1e6)
-#' lower_bounds <- c(n = 0.5, k_b = 0.1, N0 = 1e4)
-#'
-#' minimize_log = TRUE
-#'
-#' MCMC_fit <- fit_inactivation_MCMC(dynamic_inactivation, simulation_model,
-#'                                   dummy_temp, starting_points,
-#'                                   upper_bounds, lower_bounds,
-#'                                   known_params, niter = 500)
-#'
-#' MCMC_prediction <- predict_inactivation_MCMC(MCMC_fit)
-#' plot(MCMC_prediction)
-#' ## END EXAMPLE 1 -----
-#'
-predict_inactivation_MCMC <- function(MCMC_fit, n_simulations = 100,
-                                      times = NULL, quantiles = c(2.5, 97.5)) {
+predict_inactivation_MCMC <- function(fit_object, temp_profile, n_simulations = 100,
+                                      times = NULL, quantiles = c(2.5, 97.5),
+                                      additional_pars = NULL){
 
-    ## Extract fitted parameters and identify known pars
+    if (is.FitInactivationMCMC(fit_object)){
 
-    pars <- as.data.frame(MCMC_fit$modMCMC$pars)
-    fit_pars <- names(pars)
+        MCMC_sample <- sample_MCMCfit(fit_object, times, n_simulations)
 
-    best_pars <- MCMC_fit$best_prediction$model_parameters
-    good_indexes <- !(names(best_pars) %in% fit_pars)
-    known_pars <- unlist(best_pars[good_indexes])
+        par_sample <- MCMC_sample$par_sample
+        simulation_model <- MCMC_sample$simulation_model
+        known_pars = MCMC_sample$known_pars
+        times <- MCMC_sample$times
 
-    ## Extrat the rest of the model data
+    } else if (is.IsoFitInactivation(fit_object)){
 
-    simulation_model <- MCMC_fit$best_prediction$model
+        isoFit_sample <- sample_IsoFit(fit_object, times, n_simulations)
 
-    if (is.null(times)) {
-        times <- MCMC_fit$best_prediction$simulation$time
+        par_sample <- isoFit_sample$par_sample
+        simulation_model <- isoFit_sample$simulation_model
+        known_pars = isoFit_sample$known_pars
+        times <- isoFit_sample$times
+
+    } else if (is.FitInactivation(fit_object)) {
+
+        dynaFit_sample <- sample_dynaFit(fit_object, times, n_simulations)
+
+        par_sample <- dynaFit_sample$par_sample
+        simulation_model <- dynaFit_sample$simulation_model
+        known_pars = dynaFit_sample$known_pars
+        times <- dynaFit_sample$times
+
+    } else {
+        stop("fit_object must be a of class FitInactivationMCMC")
     }
 
-    temp_values <- MCMC_fit$best_prediction$temp_approximations$temp(times)
-    temp_profile <- data.frame(time = times,
-                               temperature = temp_values)
 
-    ## Sample from pars and make the simulations
-
-    par_sample <- sample_n(pars, n_simulations, replace = TRUE)
     result_matrix <- matrix(nrow = length(times), ncol = n_simulations)
 
     for (i in 1:n_simulations){
         this_pars <- unlist(par_sample[i, ])
         this_prediction <- predict_inactivation(simulation_model,
                                                 times,
-                                                c(this_pars, known_pars),
+                                                c(this_pars, known_pars, additional_pars),
                                                 temp_profile
                                                 )
         result_matrix[, i] <- this_prediction$simulation$N
@@ -120,12 +106,201 @@ predict_inactivation_MCMC <- function(MCMC_fit, n_simulations = 100,
     return(out_matrix)
 }
 
+#'
+#' Random sample of the parameters of a FitInactivationMCMC object
+#'
+#' Function to be called by \code{\link{predict_inactivation_MCMC}}. Produces
+#' a random sample of the parameters calculated on the iterations of the Monte
+#' Carlo simulation.
+#'
+#' @param MCMC_fit An object of class \code{FitInactivationMCMC} as generated
+#'        by \code{\link{fit_inactivation_MCMC}}.
+#' @param n_simulations a numeric indicating how many Monte Carlo simulations
+#'        to perform.
+#' @param times numeric vector specifying the time points when results are
+#'        desired. If \code{NULL}, the times in \code{MCMC_fit$best_prediction}
+#'        are used.
+#'
+#' @return Returns a list with 4 components.
+#'         \itemize{
+#'              \item par_sample: data frame with the parameter sample.
+#'              \item simulation_model: model key for the simulation
+#'              \item known_pars: parameters of the model known
+#'              \item times: points where the calculation is made.
+#'              }
+#'
+#' @importFrom dplyr sample_n
+#'
+sample_MCMCfit <- function(MCMC_fit, times, n_simulations){
 
+    ## Extract fitted parameters and identify known pars
 
+    pars <- as.data.frame(MCMC_fit$modMCMC$pars)
+    fit_pars <- names(pars)
 
+    best_pars <- MCMC_fit$best_prediction$model_parameters
+    good_indexes <- !(names(best_pars) %in% fit_pars)
+    known_pars <- unlist(best_pars[good_indexes])
 
+    ## Extrat the rest of the model data
 
+    simulation_model <- MCMC_fit$best_prediction$model
 
+    if (is.null(times)) {
+        times <- MCMC_fit$best_prediction$simulation$time
+    }
+
+#     temp_values <- MCMC_fit$best_prediction$temp_approximations$temp(times)
+#     temp_profile <- data.frame(time = times,
+#                                temperature = temp_values)
+
+    ## Sample from pars and make the simulations
+
+    par_sample <- sample_n(pars, n_simulations, replace = TRUE)
+
+    ## Return the results
+
+    out <- list(par_sample = par_sample,
+                simulation_model = simulation_model,
+                known_pars = known_pars,
+                times = times
+                )
+    return(out)
+}
+
+#'
+#' Random sample of the parameters of a IsoFitInactivation object
+#'
+#' Function to be called by \code{\link{predict_inactivation_MCMC}}. Produces
+#' a random sample of the parameters adjusted from isothermal experiments.
+#'
+#' It is assumed that the parameters follow a Multivariate Normal distribution
+#' with the mean and covariance matrix estimated by the adjustment. The function
+#' produces a random sample using the function \code{\link{mvrnorm}}.
+#'
+#' @param iso_fit An object of class \code{FitInactivationMCMC} as generated
+#'        by \code{\link{fit_inactivation_MCMC}}.
+#' @param n_simulations a numeric indicating how many Monte Carlo simulations
+#'        to perform.
+#' @param times numeric vector specifying the time points when results are
+#'        desired. If \code{NULL}, an equispaced interval between \code{0}
+#'        and the maximum time of the observations with length \code{50}
+#'        is used.
+#'
+#' @return Returns a list with 4 components.
+#'         \itemize{
+#'              \item par_sample: data frame with the parameter sample.
+#'              \item simulation_model: model key for the simulation
+#'              \item known_pars: parameters of the model known
+#'              \item times: points where the calculation is made.
+#'              }
+#'
+#' @importFrom MASS mvrnorm
+#'
+#' @seealso \code{\link{mvrnorm}}
+#'
+sample_IsoFit <- function(iso_fit, times, n_simulations){
+
+    ## Take the sample of the parameters
+
+    par_sample <- as.data.frame(mvrnorm(n_simulations,
+                                        coef(iso_fit$nls),
+                                        vcov(iso_fit$nls))
+                                )
+
+    ## Identify known pars
+
+    fit_pars <- names(par_sample)
+
+    best_pars <- iso_fit$parameters
+    good_indexes <- !(names(best_pars) %in% fit_pars)
+    known_pars <- unlist(best_pars[good_indexes])
+
+    ## Extrat the rest of the model data
+
+    simulation_model <- iso_fit$model
+
+    if (is.null(times)) {
+        times <- seq(0, max(iso_fit$data$time), length = 50)
+    }
+
+    ## Return the results
+
+    out <- list(par_sample = as.data.frame(par_sample),
+                simulation_model = simulation_model,
+                known_pars = known_pars,
+                times = times
+    )
+    return(out)
+
+}
+
+#'
+#' Random sample of the parameters of a FitInactivation object
+#'
+#' Function to be called by \code{\link{predict_inactivation_MCMC}}. Produces
+#' a random sample of the parameters adjusted from dynamic experiments with non
+#' linear regression.
+#'
+#' It is assumed that the parameters follow a Multivariate Normal distribution
+#' with the mean the parameters estimated by \code{\link{modFit}}. The unscaled
+#' covariance matrix returned by \code{\link{modFit}} is used.
+#'
+#' The function produces a random sample using the function \code{\link{mvrnorm}}.
+#'
+#' @param dynamic_fit An object of class \code{FitInactivationMCMC} as generated
+#'        by \code{\link{fit_inactivation_MCMC}}.
+#' @param n_simulations a numeric indicating how many Monte Carlo simulations
+#'        to perform.
+#' @param times numeric vector specifying the time points when results are
+#'        desired. If \code{NULL}, the times in \code{MCMC_fit$best_prediction}
+#'        are used.
+#'
+#' @return Returns a list with 4 components.
+#'         \itemize{
+#'              \item par_sample: data frame with the parameter sample.
+#'              \item simulation_model: model key for the simulation
+#'              \item known_pars: parameters of the model known
+#'              \item times: points where the calculation is made.
+#'              }
+#'
+sample_dynaFit <- function(dynamic_fit, times, n_simulations){
+
+    ## Take the sample of the parameters
+
+    sP <- summary(dynamic_fit)
+
+    par_sample <- as.data.frame(mvrnorm(n_simulations,
+                                        coef(dynamic_fit$fit_results),
+                                        sP$cov.unscaled)
+                                )
+
+    ## Identify known pars
+
+    fit_pars <- names(par_sample)
+
+    best_pars <- dynamic_fit$best_prediction$model_parameters
+    good_indexes <- !(names(best_pars) %in% fit_pars)
+    known_pars <- unlist(best_pars[good_indexes])
+
+    ## Extrat the rest of the model data
+
+    simulation_model <- dynamic_fit$best_prediction$model
+
+    if (is.null(times)) {
+        times <- seq(0, max(dynamic_fit$data$time), length = 50)
+    }
+
+    ## Return the results
+
+    out <- list(par_sample = as.data.frame(par_sample),
+                simulation_model = simulation_model,
+                known_pars = known_pars,
+                times = times
+                )
+    return(out)
+
+}
 
 
 
